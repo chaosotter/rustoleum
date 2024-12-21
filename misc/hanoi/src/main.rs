eba036029f15aae0788fb2627b2db321a50e1f4b
Chaosotter <meiss.mark@gmail.com>
2024-12-21 20:26:12 +0000

    cleaner version with methods on a struct

--- misc/hanoi/src/main.rs
+++ misc/hanoi/src/main.rs
@@ -1,43 +1,74 @@
 use console::Term;
 use std::io::Write;
 
-const COLORS: [&str; 7] = [
-    "\x1b[31m",
-    "\x1b[32m",
-    "\x1b[33m",
-    "\x1b[34m",
-    "\x1b[35m",
-    "\x1b[36m",
-    "\x1b[37m"
-];
+// Returns an ANSI terminal command to set the foreground color (0-7).
+fn color(color: i32) -> String {
+    return format!("\x1b[3{}m", color);
+}
 
-type Board = [Vec<i32>; 3];
+// The board consists of three stacks of discs.
+struct Board {
+    discs: i32,
+    stacks: [Vec<i32>; 3]
+}
 
-fn print_board(board: &Board) {
-    print!("\x1b[2J\x1b[H\x1b[97m");
-    for _ in 0..3 {
-        print!("         ╭╮       ");
+impl Board {
+    fn new(discs: i32) -> Self { 
+        let mut board = Self{discs: discs, stacks: [vec![], vec![], vec![]]};
+        for i in 0..discs {
+            board.stacks[0].push(i);
+        }
+        board
     }
-    println!();
 
-    for row in 0..7 {
-        for col in 0..3 {
-            if row < 7 - board[col].len() {
-                print!("\x1b[97m         ││       ");
-                continue;
-            }
-            let elem = board[col].len() - (7 - row);
-            let disc = board[col][elem];
-            for _ in 0..(7-disc+2) { print!(" "); }
-            print!("{}", COLORS[disc as usize]);
-            for _ in 0..(disc*2+2) { print!("▓"); }
-            for _ in 0..(7-disc) { print!(" "); }
+    fn draw(&self) {
+        print!("\x1b[2J\x1b[H\x1b[97m");
+        for _ in 0..3 {
+            print!("         ╭╮       ");
         }
         println!();
+
+        for row in 0..7 {
+            for col in 0..3 {
+                if row < 7 - self.stacks[col].len() {
+                    print!("\x1b[97m         ││       ");
+                    continue;
+                }
+                let elem = self.stacks[col].len() - (7 - row);
+                let disc = self.stacks[col][elem];
+                for _ in 0..(7-disc+2) { print!(" "); }
+                print!("{}", color(disc + 1));
+                for _ in 0..(disc*2+2) { print!("▓"); }
+                for _ in 0..(7-disc) { print!(" "); }
+            }
+            println!();
+        }
+        print!("\x1b[97m");
+        for _ in 0..56 { print!("▔"); }
+        println!();
     }
-    print!("\x1b[97m");
-    for _ in 0..56 { print!("▔"); }
-    println!();
+
+    fn move_disc(&mut self, from: usize, to: usize) -> bool {
+        if from == to {
+            return false;
+        } else if self.stacks[from].len() == 0 {
+            return false;
+        } else if self.stacks[to].len() > 0 {
+            let from_size = self.stacks[from][0];
+            let to_size = self.stacks[to][0];
+            if from_size > to_size {
+                return false
+            }
+        }
+
+        let disc = self.stacks[from].remove(0);
+        self.stacks[to].insert(0, disc);
+        true
+    }
+
+    fn won(&self) -> bool {
+        self.stacks[2].len() == (self.discs as usize)
+    }
 }
 
 fn get_post(term: &Term) -> Option<i32> {
@@ -65,14 +96,11 @@ fn main() {
         }
     };
 
-    let mut board: Board = [vec![], vec![], vec![]];
-    for i in 0..discs {
-        board[0].push(i);
-    }
-
+    let mut board = Board::new(discs);
     let mut term = Term::stdout();
-    while board[2].len() < (discs as usize) {
-        print_board(&board);
+
+    while !board.won() {
+        board.draw();
 
         write!(term, "\x1b[37mMove from ").expect("Terminal error");
         let from = match get_post(&term) {
@@ -86,23 +114,10 @@ fn main() {
             None => break
         };
 
-        if from == to {
-            continue;
-        } else if board[from as usize].len() == 0 {
-            continue;
-        } else if board[to as usize].len() > 0 {
-            let from_size = board[from as usize][0];
-            let to_size = board[to as usize][0];
-            if from_size > to_size {
-                continue;
-            }
-        }
-
-        let disc = board[from as usize].remove(0);
-        board[to as usize].insert(0, disc);
+        board.move_disc(from as usize, to as usize);
     }
 
-    if board[2].len() == (discs as usize) {
+    if board.won() {
         println!("\x1b[2J\x1b[H\x1b[97mYou won!");
     }
 }
